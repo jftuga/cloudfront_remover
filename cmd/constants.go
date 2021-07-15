8cc4ed58121cf0736bd4d41608e57f2f60b7d9f3
John Taylor <JohnNoSpamTaylor@gmail.com>
2021-07-15 16:03:42 -0400

    version bump

--- cmd/constants.go
+++ cmd/constants.go
@@ -1,5 +1,5 @@
 package cmd
 
 const pgmName string = "cloudfront_remover"
-const pgmVersion string = "0.8.0"
+const pgmVersion string = "0.9.0"
 const pgmURL string = "https://github.com/jftuga/cloudfront_remover"
